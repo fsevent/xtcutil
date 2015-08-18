688424e92ebc0eba282e9da4fe09133be939102a
Tanaka Akira <akr@fsij.org>
2015-08-18 18:59:04 +0900

    set window size appropriate to the layout.

--- lib/xtcutils/cmd_show.rb
+++ lib/xtcutils/cmd_show.rb
@@ -1,11 +1,10 @@
 require 'gtk3'
 
-require 'xtcutils/parser'
+require 'xtcutils'
 
 class XTCWindow < Gtk::Window
-  def initialize(params, parsed)
-    @params = params
-    @parsed = parsed
+  def initialize(layout)
+    @layout = layout
 
     super()
 
@@ -20,9 +19,22 @@ class XTCWindow < Gtk::Window
     end
     add @drawingarea
 
-    set_default_size 400, 200
+    calc_scale
+    set_default_size @window_w, @window_h
     set_window_position :center
     show_all
+  end
+
+  def calc_scale
+    w, h = @layout.roomsize
+    return 100, 100 if !w
+    max_w = 700
+    max_h = 500
+    scale_x = max_w / w
+    scale_y = max_h / h
+    @scale = [scale_x, scale_y].min
+    @window_w = (w * @scale).to_i
+    @window_h = (h * @scale).to_i
   end
 
   def draw_callback
@@ -32,9 +44,10 @@ class XTCWindow < Gtk::Window
 
   def draw_xtc ctx
     ctx.save {
-      ctx.scale 10, 10
+      ctx.translate 0, @window_h
+      ctx.scale @scale, -@scale
       ctx.set_line_width 1
-      @parsed.each {|h|
+      @layout.parsed.each {|h|
         if h[:type] == 'straight'
           x1, y1 = h[:segs][0][:pos]
           x2, y2 = h[:segs][1][:pos]
@@ -55,6 +68,6 @@ def main_show(argv)
     parse_io params, parsed, f
   }
   Gtk.init
-  window = XTCWindow.new(params, parsed)
+  window = XTCWindow.new(Layout.new(parsed))
   Gtk.main
 end
--- lib/xtcutils/layout.rb
+++ lib/xtcutils/layout.rb
@@ -3,4 +3,14 @@ class Layout
     @parsed = parsed
   end
   attr_reader :parsed
+
+  def roomsize
+    return @roomsize if defined? @roomsize
+    @parsed.each {|h|
+      if h[:type] == 'roomsize'
+        return @roomsize = [h[:x], h[:y]]
+      end
+    }
+    return @roomsize = nil
+  end
 end
