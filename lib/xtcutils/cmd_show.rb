acb3920ebc5932e43c0ddcac6a3e247afaa41cd8
Tanaka Akira <akr@fsij.org>
2015-08-18 17:53:32 +0900

    add "show" subcommand to display the layout.

---
+++ lib/xtcutils/cmd_show.rb
@@ -0,0 +1,60 @@
+require 'gtk3'
+
+require 'xtcutils/parser'
+
+class XTCWindow < Gtk::Window
+  def initialize(params, xtc)
+    @params = params
+    @xtc = xtc
+
+    super()
+
+    set_title "xtcutils"
+    signal_connect "destroy" do
+      Gtk.main_quit
+    end
+
+    @drawingarea = Gtk::DrawingArea.new
+    @drawingarea.signal_connect "draw" do
+      draw_callback
+    end
+    add @drawingarea
+
+    set_default_size 400, 200
+    set_window_position :center
+    show_all
+  end
+
+  def draw_callback
+    ctx = @drawingarea.window.create_cairo_context
+    draw_xtc ctx
+  end
+
+  def draw_xtc ctx
+    ctx.save {
+      ctx.scale 10, 10
+      ctx.set_line_width 1
+      @xtc.each {|h|
+        if h[:type] == 'straight'
+          x1, y1 = h[:segs][0][:pos]
+          x2, y2 = h[:segs][1][:pos]
+          ctx.move_to(x1, y1)
+          ctx.line_to(x2, y2)
+          ctx.stroke
+        end
+      }
+    }
+  end
+end
+
+
+def main_show(argv)
+  params = {}
+  xtc = []
+  open_xtc(argv[0]) {|f|
+    parse_io params, xtc, f
+  }
+  Gtk.init
+  window = XTCWindow.new(params, xtc)
+  Gtk.main
+end
