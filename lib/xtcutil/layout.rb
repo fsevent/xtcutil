e810b98eae122d2767b4cf4d3b9ac5af4ac28c24
Tanaka Akira <akr@fsij.org>
2015-09-02 16:44:06 +0900

    use radian.

--- lib/xtcutil/layout.rb
+++ lib/xtcutil/layout.rb
@@ -85,7 +85,7 @@ class Layout
         node = Node.new
         node.add_comment "T#{obj.index}EP#{ep_num}#{ep[:type]}"
         node.add_list_attr :ep_pos, ep[:pos]
-        node.add_list_attr :ep_angle, ep[:angle]
+        node.add_list_attr :ep_angle, ep[:angle] * DEG_TO_RAD
         obj.set_endpoint_node ep, node
         if ep[:station_name] && /\S/ =~ ep[:station_name]
           node.set_node_name(ep[:station_name].strip.gsub(/\s+/, '_'))
@@ -111,12 +111,12 @@ class Layout
           raise "too many node at #{ep_pos_list[0].inspect}"
         end
         angle1, angle2 = node.get_list_attr(:ep_angle)
-        tolerance = 45.0
+        tolerance = 45.0 * DEG_TO_RAD
         a1, a2 = angle1, angle2
-        a1 += 180 # opposite direction
-        a1 -= 360 if 360 <= a1
+        a1 += Math::PI # opposite direction
+        a1 -= 2 * Math::PI if 2 * Math::PI <= a1
         d = (a1 - a2).abs
-        if tolerance < d && tolerance < (d - 360).abs
+        if tolerance < d && tolerance < (d - 2 * Math::PI).abs
           raise "inter part node angle too bend: #{angle1} #{angle2}"
         end
       }
